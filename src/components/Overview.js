f4385c635ba27cd907189dbd3f5c3219f795f622
Matin Timi <martintimi2443@gmail.com>
2024-06-13 11:36:47 +0100

    updated

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,11 +1,11 @@
 import { Container, Row, Col } from "react-bootstrap";
-import AppImg from "../assets/images/app-overview2.png"
+import AppImg from "../assets/images/app-overview2.png";
 
 function Overview() {
   return (
     <section className="overview" id="overview">
       <Container>
-        <Row  className="align-items-center">
+        <Row className="align-items-center">
           <Col xs={12} md={6} xl={5}>
             <span className="text-primary">APP OVERVIEW</span>
             <h1 className="header-with-line pt-3">
@@ -15,9 +15,9 @@ function Overview() {
               We're a digital product and UX agency Strategy, design and
               development across all platforms.
             </p>
-            <div className="d-flex py-3 ">
+            <div className="d-flex py-3">
               <button className="btn1">Payment</button>
-              <button className="btn2 ">Credit Card</button>
+              <button className="btn2">Credit Card</button>
               <button className="btn2">Location</button>
             </div>
             <h5 className="pt-3">Payment Request</h5>
@@ -32,9 +32,8 @@ function Overview() {
               <li>Fun is an absolute must</li>
             </ul>
           </Col>
-          <Col xs={12} md={3} xl={3}/>
-          <Col xs={12} md={3} xl={4}>
-            <img src={AppImg} alt="" className="mb-5"/>
+          <Col xs={12} md={6} xl={7} className="text-center">
+            <img src={AppImg} alt="App Overview" className="mb-5"/>
           </Col>
         </Row>
       </Container>
